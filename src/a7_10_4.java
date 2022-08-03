547aee4e48943466275dfc6f102ec14a93d382af
kdy991101 <kdy991101@naver.com>
2022-08-03 09:00:56 +0900

    ty

--- src/a7_10_4.java
+++ src/a7_10_4.java
@@ -3,24 +3,25 @@ import java.util.Scanner;
 public class a7_10_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//			최대 100까지의 정수를 차례로 입력받다가 0이 입력되면 입력을 중단하고 짝수번째에 입력된 정수를 모두 출력
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int[] arr = new int [100];
-		int cnt = 0;
-		
-		for(int i = 0; i<arr.length; i++) {
-			arr[i] = sc.nextInt();
-			if(arr[i] == 0) {
-				break;
-			}
-			cnt++;
-		}
-		for(int i = 1; i<cnt; i+=2) {
-			System.out.println(arr[i] + "");
-		}
+		byte b = 10;
+
+		short s = 10;
+
+		char ch = 'A';
+
+		int i = 10;
+
+		long l = 10L;
+
+		float f =10;
+
+		     
+
+		byte b2 = (byte)i;
+		float f2 = (float)l;
+			int i2 =(int)l;
+			char ch2=(char)i;
+			short s2 = (short)ch;
 	}
 
 }
